e88ac55c2a3cafa6698b9834272f76c705484049
chhong <pnci1029@gmail.com>
2026-02-15 15:34:26 +0900

    [moodbite] 선호도 결과 로직 완료

--- moodbite/MoodBite_FE/src/components/api/TestApi.ts
+++ moodbite/MoodBite_FE/src/components/api/TestApi.ts
@@ -1,9 +1,12 @@
-import {TestResultPostDTO} from "../../types/test";
+import {TestResultPostDTO, LocationBasedTestResultRequestDTO} from "../../types/test";
 import {MainApi} from "./MainApi";
 
 export class TestApi {
     static url = `${process.env.REACT_APP_BASE_URL}/mood-test`;
 
-    static submitTestResult = (dto:TestResultPostDTO) => () =>
+    static submitTestResult = (dto: TestResultPostDTO) => () =>
         MainApi.api.post(`${TestApi.url}`, dto);
+
+    static submitLocationBasedTestResult = (dto: LocationBasedTestResultRequestDTO) => () =>
+        MainApi.api.post(`${TestApi.url}/location-based`, dto);
 }
--- moodbite/MoodBite_FE/src/slice/testSlice.ts
+++ moodbite/MoodBite_FE/src/slice/testSlice.ts
@@ -1,5 +1,10 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {TestResultPostDTO, TestResultResponseDTO} from "../types/test";
+import {
+    TestResultPostDTO, 
+    TestResultResponseDTO, 
+    LocationBasedTestResultRequestDTO, 
+    LocationBasedRecommendationResponseDTO
+} from "../types/test";
 import {executePromise} from "./index";
 import {TestApi} from "../components/api/TestApi";
 
@@ -7,6 +12,14 @@ export const submitTestResultAsync = createAsyncThunk<TestResultResponseDTO, Tes
     "test/submitTestResult",
     async (dto: TestResultPostDTO) => {
         const response = await executePromise(TestApi.submitTestResult(dto));
+        return response.data;
+    }
+);
+
+export const submitLocationBasedTestResultAsync = createAsyncThunk<LocationBasedRecommendationResponseDTO, LocationBasedTestResultRequestDTO>(
+    "test/submitLocationBasedTestResult",
+    async (dto: LocationBasedTestResultRequestDTO) => {
+        const response = await executePromise(TestApi.submitLocationBasedTestResult(dto));
         return response.data;
     }
 );
