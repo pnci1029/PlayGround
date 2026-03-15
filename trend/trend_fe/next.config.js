a5b41b136035a85e5e788d3015a4746ecfe34a4c
chhong <pnci1029@gmail.com>
2026-03-15 11:09:55 +0900

    [main] 배포 마이그레이션 경로 수정

--- trend/trend_fe/next.config.js
+++ trend/trend_fe/next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
   async headers() {
     return [
       {
