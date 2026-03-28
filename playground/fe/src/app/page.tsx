bb8e9f3b072d981e6cda8e30b34040494c7cadf7
chhong <pnci1029@gmail.com>
2026-03-28 13:53:09 +0900

    [main] 프론트 소켓 요청 테스트

--- playground/fe/src/app/page.tsx
+++ playground/fe/src/app/page.tsx
@@ -4,7 +4,7 @@ import ToolCategoryGrid from '@/components/HomePage/ToolCategoryGrid'
 import FooterSection from '@/components/HomePage/FooterSection'
 
 export default function HomePage() {
-    console.log('test1')
+    console.log('test3')
   return (
     <div className="min-h-screen bg-white">
       {/* Hero Section */}
