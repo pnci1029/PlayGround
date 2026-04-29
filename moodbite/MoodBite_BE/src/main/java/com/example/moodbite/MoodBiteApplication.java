741828e934b1e1087a75ad0052d47d0616db6d75
chhong <pnci1029@gmail.com>
2026-04-29 21:37:57 +0900

    [main] 배포테스트 3

--- moodbite/MoodBite_BE/src/main/java/com/example/moodbite/MoodBiteApplication.java
+++ moodbite/MoodBite_BE/src/main/java/com/example/moodbite/MoodBiteApplication.java
@@ -7,8 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoodBiteApplication {
 
     public static void main(String[] args) {
-        System.out.println("🍽️ MoodBite Application 개별 배포 테스트 시작! v1.1");
-        System.out.println("🚀 fetch-depth 수정 후 재테스트!");
+        System.out.println("🍽️ MoodBite Application 개별 배포 테스트 시작! v1.2");
+        System.out.println("🚀 grep 패턴 수정 후 재테스트!");
+        System.out.println("✅ 이제 MoodBite만 재시작되길 기대!");
         SpringApplication.run(MoodBiteApplication.class, args);
     }
 
