673deb50133d092d78c75bbe8bf051e8f8ffdf39
chhong <pnci1029@gmail.com>
2026-05-18 20:25:04 +0900

    [main] 배포테스트

--- playground/fe/src/app/page.tsx
+++ playground/fe/src/app/page.tsx
@@ -4,7 +4,7 @@ import ToolCategoryGrid from '@/components/HomePage/ToolCategoryGrid'
 import FooterSection from '@/components/HomePage/FooterSection'
 
 export default function HomePage() {
-    console.log('test8')
+    console.log('test9')
   return (
     <div className="min-h-screen bg-white">
       {/* Hero Section */}
