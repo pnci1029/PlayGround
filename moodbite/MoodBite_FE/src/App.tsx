ee0f58b9e766e8ff8e06de63f586b490558148f5
chhong <pnci1029@gmail.com>
2026-06-18 14:14:20 +0900

    [moodbite] 불필요한 소스 제거

--- moodbite/MoodBite_FE/src/App.tsx
+++ moodbite/MoodBite_FE/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import {Outlet} from "react-router-dom";
 
 function App() {
-  console.log('git email test');
   return (
       <div className="App">
           <Outlet />
--- moodbite/MoodBite_FE/src/index.tsx
+++ moodbite/MoodBite_FE/src/index.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import {Main} from "./components/common/Main";
 import App from "./App";
 import { TestResultPage } from "./components/test/TestResultPage";
+import { Test } from "./components/test/Test";
+import { NearbyRestaurants } from "./components/location/NearbyRestaurants";
+
+// 라우트 진입점: 기존 onBack 인터페이스를 라우터 이동으로 연결한다.
+function TestRoute() {
+    const navigate = useNavigate();
+    return <Test onBack={() => navigate('/')} />;
+}
+
+function NearbyRoute() {
+    const navigate = useNavigate();
+    return <NearbyRestaurants onBack={() => navigate('/')} />;
+}
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -16,7 +29,9 @@ root.render(
             <Routes>
                 <Route path="/" element={<App/>}>
                     <Route path="" element={<Main/>} />
+                    <Route path="test" element={<TestRoute/>} />
                     <Route path="test/result" element={<TestResultPage/>} />
+                    <Route path="nearby" element={<NearbyRoute/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
